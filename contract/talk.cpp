897c3032321debb49cdbd69acba5106a892ee93c
Eric Ma <zma@ericzma.com>
2020-05-07 08:43:54 +0000

    Add likes table

--- contract/talk.cpp
+++ contract/talk.cpp
@@ -15,6 +15,16 @@ struct [[eosio::table("message"), eosio::contract("talk")]] message {
 using message_table = eosio::multi_index<
     "message"_n, message, eosio::indexed_by<"by.reply.to"_n, eosio::const_mem_fun<message, uint64_t, &message::get_reply_to>>>;
 
+struct [[eosio::table("likes"), eosio::contract("talk")]] likes {
+    uint64_t    id       = {}; // Non-0
+    std::vector<eosio::name> likedby = std::vector<eosio::name>{};
+
+    uint64_t primary_key() const { return id; }
+};
+
+using likes_table = eosio::multi_index<
+    "likes"_n, likes>;
+
 // The contract
 class talk : eosio::contract {
   public:
@@ -24,6 +34,7 @@ class talk : eosio::contract {
     // Post a message
     [[eosio::action]] void post(uint64_t id, uint64_t reply_to, eosio::name user, const std::string& content) {
         message_table table{get_self(), 0};
+        likes_table likestable{get_self(), 0};
 
         // Check user
         require_auth(user);
@@ -43,6 +54,39 @@ class talk : eosio::contract {
             message.reply_to = reply_to;
             message.user     = user;
             message.content  = content;
+            message.likes    = 0;
+        });
+
+        likestable.emplace(get_self(), [&](auto& likes) {
+            likes.id = id;
+            likes.likedby = std::vector<eosio::name>{};
+        });
+    }
+
+    // Like a message
+    [[eosio::action]] void like(uint64_t id, eosio::name user) {
+        message_table table{get_self(), 0};
+        likes_table likestable{get_self(), 0};
+
+        // Check user
+        require_auth(user);
+
+        // Create an ID if user didn't specify one
+        eosio::check(id < 1'000'000'000ull, "user-specified id is too big");
+
+        auto itr = table.find(id);
+        eosio::check(itr != table.end(), "message does not exist!");
+
+        auto likesitr = likestable.find(id);
+        eosio::check(likesitr != likestable.end(), "message does not exist in likes_table!");
+
+        // add like the message
+        table.modify(itr, get_self(), [&](auto& message) {
+            message.likes    += 1;
+        });
+
+        likestable.modify(likesitr, get_self(), [&](auto& row) {
+            row.likedby.push_back(user);
         });
     }
 };
